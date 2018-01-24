505232e9898f56ca50a9106f70c23f3ecf783e69
KaoWebDesign <assdwqe@gmail.com>
2018-01-24 20:11:09 +0200

    Full add Menu and creativity block

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     var navk = $('#nav');
     var navBarsk = $('#navBars');
     var menuK = $('#menu');
+    if (window.scrollY > 80 && menuK.hasClass('header__menu-active')) {
+        navk.addClass('nav-active');
+    } else navk.removeClass('nav-active');
     $(window).scroll(function() {
         if (window.scrollY > 80 && menuK.hasClass('header__menu-active')) {
             navk.addClass('nav-active');
@@ -9,8 +12,10 @@ $(document).ready(function() {
     });
     navBarsk.on('click', function(e) {
         e.preventDefault();
-        navk.toggleClass('nav-active');
         navBarsk.toggleClass('nav__bar-active');
         menuK.toggleClass('header__menu-active');
+        if (window.scrollY > 80 && menuK.hasClass('header__menu-active')) {
+            navk.addClass('nav-active');
+        } else navk.removeClass('nav-active');
     })
 });
