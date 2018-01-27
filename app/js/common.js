4aacde917fc646ea490376728cd7751ad1fa18a7
KaoWebDesign <assdwqe@gmail.com>
2018-01-27 23:33:44 +0200

    add some part of About me

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var navk = $('#nav');
     var navBarsk = $('#navBars');
     var menuK = $('#menu');
+    var clicker = $('.clicker');
     if (window.scrollY > 80 && menuK.hasClass('header__menu-active')) {
         navk.addClass('nav-active');
     } else navk.removeClass('nav-active');
@@ -10,12 +11,12 @@ $(document).ready(function() {
             navk.addClass('nav-active');
         } else navk.removeClass('nav-active');
     });
-    navBarsk.on('click', function(e) {
+    clicker.on('click', function(e) {
         e.preventDefault();
         navBarsk.toggleClass('nav__bar-active');
         menuK.toggleClass('header__menu-active');
         if (window.scrollY > 80 && menuK.hasClass('header__menu-active')) {
             navk.addClass('nav-active');
         } else navk.removeClass('nav-active');
-    })
+    });
 });
